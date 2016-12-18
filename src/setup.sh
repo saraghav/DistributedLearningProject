55c47c4abcd458888c0679f404140c9ed8e04461
saraghav <araghav.s@gmail.com>
2016-12-18 14:22:25 -0600

    dependency installation on linux

---
+++ src/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+apt-get --assume-yes install python3 python3-pip python3-numpy python3-scipy python3-matplotlib
+
+# Ubuntu/Linux 64-bit, CPU only, Python 3.4
+export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.12.0rc1-cp34-cp34m-linux_x86_64.whl
+pip3 install --upgrade $TF_BINARY_URL
